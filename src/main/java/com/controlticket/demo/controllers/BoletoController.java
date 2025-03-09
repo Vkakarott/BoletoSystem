5c0360806bcd2695720e26be23a2f6f2d2c54c8f
Vkakarott <vecctor.vitor15@gmail.com>
2025-03-09 20:41:59 -0300

    feat: params

--- src/main/java/com/controlticket/demo/controllers/BoletoController.java
+++ src/main/java/com/controlticket/demo/controllers/BoletoController.java
@@ -1,53 +1,74 @@
 package com.controlticket.demo.controllers;
 
+import com.controlticket.demo.models.Ticket;
+import com.controlticket.demo.services.TicketService;
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/boleto")
 public class BoletoController {
 
+    @Autowired
+    private TicketService ticketService;
+
     @GetMapping("/gerar")
-    public ResponseEntity<byte[]> gerarBoleto() {
+    public ResponseEntity<byte[]> gerarBoleto(@RequestParam("id") Long id) {
         try {
+            Optional<Ticket> tickOptional = ticketService.findTicketById(id);
+            if (!tickOptional.isPresent()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            Ticket ticket = tickOptional.get();
+
+            DecimalFormat valorFormatado = new DecimalFormat("###,##0.00");
+            String valorBoleto = "R$ " + valorFormatado.format(ticket.getValue());
+
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate dataVencimento = ticket.getDueDate();
+
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             PdfWriter writer = new PdfWriter(outputStream);
             PdfDocument pdfDocument = new PdfDocument(writer);
             Document document = new Document(pdfDocument);
 
-            // Cabeçalho do Boleto
-            document.add(new Paragraph("BOLETO BANCÁRIO"));
+            document.add(new Paragraph("BOLETO BANCÁRIO").setBold().setTextAlignment(TextAlignment.CENTER));
             document.add(new Paragraph("==================================="));
 
-            // Informações do pagador
-            document.add(new Paragraph("Pagador: João da Silva"));
-            document.add(new Paragraph("CPF: 123.456.789-00"));
-            document.add(new Paragraph("Endereço: Rua Exemplo, 123 - Cidade/UF"));
+            document.add(new Paragraph("Pagador: " + ticket.getPayMaster().getName()));
+            document.add(new Paragraph("CPF: " + ticket.getPayMaster().getCpf()));
 
             document.add(new Paragraph("==================================="));
 
-            // Informações do boleto
-            document.add(new Paragraph("Banco: Banco Exemplo S.A."));
-            document.add(new Paragraph("Agência: 1234-5  Conta: 67890-1"));
-            document.add(new Paragraph("Valor: R$ 250,00"));
-            document.add(new Paragraph("Vencimento: 28/02/2025"));
-            document.add(new Paragraph("Linha Digitável: 12345.67890 12345.678901 12345.678901 5 12345678901234"));
+            document.add(new Paragraph("Banco: " + ticket.getBeneficiary().getName()));
+            document.add(new Paragraph("Agência: " + ticket.getBeneficiary().getId() + " Conta: " + ticket.getBeneficiary().getId()));
+            document.add(new Paragraph("Valor: " + valorBoleto));
+            document.add(new Paragraph("Vencimento: " + dataVencimento.format(dateFormatter)));
+            
+            document.add(new Paragraph("Linha Digitável: " + ticket.getCode()));
 
             document.add(new Paragraph("==================================="));
-            document.add(new Paragraph("Este é um boleto fictício para fins de demonstração."));
 
             document.close();
 
-            // Retornar o PDF como resposta HTTP
             byte[] pdfBytes = outputStream.toByteArray();
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Disposition", "inline; filename=boleto.pdf");
