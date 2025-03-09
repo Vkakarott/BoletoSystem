ab4d8d1e7a48bec5870cc85e0640dcc058b79f46
Vkakarott <vecctor.vitor15@gmail.com>
2025-03-09 20:42:36 -0300

    fix: Querys

--- src/main/java/com/controlticket/demo/repositories/TicketRepository.java
+++ src/main/java/com/controlticket/demo/repositories/TicketRepository.java
@@ -3,6 +3,8 @@ package com.controlticket.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.controlticket.demo.models.Ticket;
@@ -10,7 +12,9 @@ import com.controlticket.demo.models.Ticket;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     
-    List<Ticket> findByPayMaster(Long payMasterId);
+    @Query("SELECT t FROM Ticket t WHERE t.payMaster.id = :payMasterId")
+    List<Ticket> findByPayMaster(@Param("payMasterId") Long payMasterId);
 
-    List<Ticket> findByBeneficiary(Long beneficiaryId);
+    @Query("SELECT t FROM Ticket t WHERE t.beneficiary.id = :beneficiaryId")
+    List<Ticket> findByBeneficiary(@Param("beneficiaryId") Long beneficiaryId);
 }
