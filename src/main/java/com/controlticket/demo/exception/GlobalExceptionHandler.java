257b70cec9049d6f1edd4be9849c4935dc4f39d6
Vkakarott <vecctor.vitor15@gmail.com>
2025-03-09 15:22:23 -0300

    feat: exeptions fro data duplicate

--- src/main/java/com/controlticket/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/controlticket/demo/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -12,6 +13,27 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDuplicateEntry(DataIntegrityViolationException ex) {
+        String message = "CPF ou e-mail já cadastrados";
+
+        if (ex.getCause() != null && ex.getCause().getCause() != null) {
+            String detailedMessage = ex.getCause().getCause().getMessage();
+            if (detailedMessage.contains("clients_email_key")) {
+                message = "O e-mail informado já está cadastrado.";
+            } else if (detailedMessage.contains("clients_cpf_key")) {
+                message = "O CPF informado já está cadastrado.";
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Dados duplicados");
+        response.put("message", message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGenericException(Exception ex) {
         Map<String, Object> response = new HashMap<>();
@@ -19,6 +41,7 @@ public class GlobalExceptionHandler {
         response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         response.put("error", "Erro interno");
         response.put("message", ex.getMessage());
+        response.put("details", ex.getStackTrace()[0].toString());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
