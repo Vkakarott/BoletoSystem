7ab426d2a78128012cfba3a1508056720dad6344
Vkakarott <vecctor.vitor15@gmail.com>
2025-03-09 15:36:04 -0300

    feat: create model institution

---
+++ src/main/java/com/controlticket/demo/models/Institution.java
@@ -0,0 +1,46 @@
+package com.controlticket.demo.models;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "institutions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String cnpj;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToMany(mappedBy = "intituition", cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
+}
